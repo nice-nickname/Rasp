afe80faff0062d4d78fa0747b769d673be4445e0
Semen Makarov <bethesda.stillxxxx@gmail.com>
2023-02-13 22:45:52 +0300

    Initialize finished

--- Domain/Bootstrap.cs
+++ Domain/Bootstrap.cs
@@ -9,30 +9,6 @@ using Incoding.Core.Data;
 
 namespace Domain
 {
-    public class Roles : IncEntityBase
-    {
-        public new virtual int Id { get; set; }
-
-        public virtual string? Role { get; set; }
-
-        public class Map : ClassMap<Roles>
-        {
-            public Map()
-            {
-                Table("Roles");
-                Id(s => s.Id, "idRole");
-                Map(s => s.Role, "role");
-            }
-        }
-    }
-
-    public class Inc : QueryBase<int>
-    {
-        protected override int ExecuteResult()
-        {
-            return 123;
-        }
-    }
 
     public class Bootstrap
     {
--- Rasp/Program.cs
+++ Rasp/Program.cs
@@ -10,18 +10,78 @@ using Incoding.Core.Data;
 using Incoding.Data.EF;
 using Incoding.Web;
 using Incoding.Web.MvcContrib;
-using Microsoft.AspNetCore.Routing;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.Extensions.Caching.Memory;
 using NUglify.JavaScript;
 
 namespace Rasp
 {
-    public static class Extentions
+    public static class Startup
     {
-        public static IApplicationBuilder UseMvd(this WebApplication app)
+        public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
         {
-            return app.UseEndpoints(routeBuilder =>
+            builder.Configuration.AddJsonFile("dbconfig.json", false, true);
+
+            builder.Services
+                   .AddRazorPages()
+                   .AddRazorRuntimeCompilation();
+
+            builder.Services.AddRouting();
+
+            builder.Services
+                   .AddMvc(o => o.Filters.Add(new IncodingErrorHandlingFilter()))
+                   .AddFluentValidation(config =>
+                   {
+                       config.ValidatorFactory = new IncValidatorFactory();
+                       AssemblyScanner.FindValidatorsInAssembly(typeof(Domain.Bootstrap).Assembly);
+                   });
+
+            builder.Services.ConfigureIncodingCoreServices();
+
+            builder.Services.ConfigureIncodingNhDataServices(typeof(IncEntityBase), null, b =>
+            {
+                var db = MsSqlConfiguration.MsSql2012.ConnectionString(builder.Configuration["ConnectionString"]).ShowSql();
+                b = b.Database(db).Mappings(m => m.FluentMappings.AddFromAssembly(typeof(Domain.Bootstrap).Assembly));
+                return b;
+            });
+
+            builder.Services.ConfigureIncodingWebServices();
+
+            builder.Services.AddWebOptimizer(pipeline =>
+            {
+                // Main libraries
+                pipeline.AddJavaScriptBundle("/lib/jq.js", new CodeSettings { MinifyCode = true },
+                            "node_modules/jquery/dist/jquery.min.js",
+                            "wwwroot/lib/jquery.history.js",
+                            "node_modules/**/dist/jquery.form.min.js",
+                            "node_modules/**/dist/jquery.validate.js",
+                            "node_modules/**/dist/jquery.validate.unobtrusive.min.js",
+                            "node_modules/**/dist/jquery.history.min.js",
+                            "node_modules/underscore/underscore-min.js",
+                            "wwwroot/lib/incoding.framework.js",
+                            "node_modules/**/dist/handlebars.min.js")
+                        .UseContentRoot();
+            });
+
+            return builder.Build();
+        }
+
+        public static WebApplication ConfigureApplication(this WebApplication app)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseWebOptimizer();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(routeBuilder =>
             {
                 routeBuilder.MapControllerRoute("incodingCqrsQuery", "Cqrs/Query/{incType}", new
                 {
@@ -50,92 +110,22 @@ namespace Rasp
                 });
                 routeBuilder.MapDefaultControllerRoute();
             });
+
+            IoCFactory.Instance.Initialize(ioc => ioc.WithProvider(new MSDependencyInjectionIoCProvider(app.Services)));
+            CachingFactory.Instance.Initialize(cache => cache.WithProvider(new NetCachedProvider(() => app.Services.GetRequiredService<IMemoryCache>())));
+
+            return app;
         }
     }
 
     public class Program
     {
-        private static void ConfigureServices(WebApplicationBuilder builder)
-        {
-            builder.Services.AddRazorPages();
-            builder.Services.AddRouting();
-
-            builder.Services
-                   .AddMvc(o =>
-                   {
-                       o.Filters.Add(new IncodingErrorHandlingFilter());
-                   })
-                   .AddFluentValidation(c =>
-                   {
-                       c.ValidatorFactory = new IncValidatorFactory();
-                       AssemblyScanner.FindValidatorsInAssembly(typeof(Domain.Bootstrap).Assembly);
-                   });
-
-            builder.Services.ConfigureIncodingCoreServices();
-
-            builder.Services.ConfigureIncodingNhDataServices(typeof(IncEntityBase), null, b =>
-            {
-                var connectionString = @"Server=localhost,1433;Database=News;User Id=ahaha;Password=123;";
-				
-				var db = MsSqlConfiguration.MsSql2012.ConnectionString(connectionString).ShowSql();
-                b = b.Database(db).Mappings(m => m.FluentMappings.AddFromAssembly(typeof(Domain.Bootstrap).Assembly));
-                return b;
-            });
-
-            builder.Services.ConfigureIncodingWebServices();
-
-            builder.Services.AddWebOptimizer(pipeline =>
-            {
-                // Main libraries
-                pipeline.AddJavaScriptBundle("/lib/jq.js", new CodeSettings { MinifyCode = true },
-                            "node_modules/jquery/dist/jquery.min.js",
-                            "wwwroot/lib/jquery.history.js",
-                            "node_modules/**/dist/jquery.form.min.js",
-                            "node_modules/**/dist/jquery.validate.js",
-                            "node_modules/**/dist/jquery.validate.unobtrusive.min.js",
-                            "node_modules/**/dist/jquery.history.min.js",
-                            "node_modules/underscore/underscore-min.js",
-                            "wwwroot/lib/incoding.framework.js",
-                            "node_modules/**/dist/handlebars.min.js")
-                        .UseContentRoot();
-            });
-        }
-
-        private static void Configure(WebApplication app)
-        {
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
-
-            app.UseWebOptimizer();
-            app.UseStaticFiles();
-            
-            app.UseRouting();
-            app.UseAuthorization();
-            app.UseMvd();
-
-            IoCFactory.Instance.Initialize(i => i.WithProvider(new MSDependencyInjectionIoCProvider(app.Services)));
-            CachingFactory.Instance.Initialize(init =>
-                init.WithProvider(new NetCachedProvider(() => app.Services.GetRequiredService<IMemoryCache>())));
-        }
-
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-            ConfigureServices(builder);
-
-            var app = builder.Build();
-            Configure(app);
-
-            IoCFactory.Instance.Initialize(init => init.WithProvider(new MSDependencyInjectionIoCProvider(app.Services)));
-            CachingFactory.Instance.Initialize(init => init.WithProvider(new NetCachedProvider(() => app.Services.GetRequiredService<IMemoryCache>())));
-
-            app.Run();
+            WebApplication.CreateBuilder(args)
+                          .ConfigureServices()
+                          .ConfigureApplication()
+                          .Run();
         }
     }
 }
