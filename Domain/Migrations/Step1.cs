95f03933a084938671bf7828e11b429443f88843
Semen Makarov <bethesda.stillxxxx@gmail.com>
2023-02-16 10:04:57 +0300

    Created tables for Auditorium and Building

--- Domain/Migrations/Step1.cs
+++ Domain/Migrations/Step1.cs
@@ -6,14 +6,26 @@ namespace Domain.Migrations
 	public class Step1 : Migration
 	{
 		public override void Up()
-		{
-			Execute.Sql("SELECT 1");
-		}
+        {
+            Create.Table("Building")
+                  .WithColumn("Code").AsFixedLengthString(1).PrimaryKey();
+
+            Create.Table("Auditorium")
+                  .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                  .WithColumn("Building").AsFixedLengthString(1)
+                  .WithColumn("Code").AsFixedLengthString(3);
+
+            Create.ForeignKey("FK_Building_Auditoriums")
+                  .FromTable("Auditorium")
+                  .ForeignColumn("Building")
+                  .ToTable("Building")
+                  .PrimaryColumn("Code");
+        }
 
 		public override void Down()
-		{
-			
-		}
-
+        {
+            Delete.Table("Auditorium");
+            Delete.Table("Building");
+        }
 	}
 }
