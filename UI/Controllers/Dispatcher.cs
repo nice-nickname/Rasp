fcfb35921ee6af138689c9f8298320534c1f8a29
Semen Makarov <bethesda.stillxxxx@gmail.com>
2023-03-01 10:07:30 +0300

    Renamed Project files

---
+++ UI/Controllers/Dispatcher.cs
@@ -0,0 +1,12 @@
+﻿using Incoding.Web.MvcContrib;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UI.Controllers
+{
+    public class Dispatcher : DispatcherControllerBase
+    {
+        public Dispatcher(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+        }
+    }
+}
---
+++ UI/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Bass()
+        {
+            return Json(new {test = "1"});
+        }
+    }
+}
