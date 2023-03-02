aa7203de91020bb83b26885fb710e46f62398be8
Semen Makarov <bethesda.stillxxxx@gmail.com>
2023-03-03 01:29:34 +0300

    Code cleanup

--- UI/Controllers/HomeController.cs
+++ UI/Controllers/HomeController.cs
@@ -4,22 +4,9 @@ namespace UI.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
-        }
-
-
-        public IActionResult Bass()
-        {
-            return Json(new {test = "1"});
         }
     }
 }
---
+++ UI/Controllers/MigrationsController.cs
@@ -0,0 +1,33 @@
+﻿using FluentMigrator.Runner;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UI.Controllers;
+
+[Route("migrator")]
+public class MigrationsController : Controller
+{
+    private readonly IMigrationRunner _migrator;
+
+    public MigrationsController(IMigrationRunner migrator)
+    {
+        _migrator = migrator;
+    }
+
+    [Route("migrate")]
+    public void Migrate()
+    {
+        this._migrator.MigrateUp();
+    }
+
+    [Route("up/{number:long}")]
+    public void Up(long number)
+    {
+        this._migrator.MigrateUp(number);
+    }
+
+    [Route("down/{number:long}")]
+    public void Down(long number)
+    {
+        this._migrator.MigrateDown(number);
+    }
+}
