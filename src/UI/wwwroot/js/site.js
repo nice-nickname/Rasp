413b3e59bb7a07c8250b8f624c2a6bc0984cb552
semen <bethesda.stillxxxx@gmail.com>
2023-04-12 11:49:54 +0300

    SC-95: Генерация предложений по вводу института

--- src/UI/wwwroot/js/site.js
+++ src/UI/wwwroot/js/site.js
@@ -1,1 +1,13 @@
-﻿
+﻿/// <reference path="../../node_modules/jquery/dist/jquery.js" />
+
+function suggestInputValueByNamingCase(value, to) {
+    if (!value) {
+        return;
+    }
+    const val = value.split(' ')
+        .filter(s => s && s.length > 1)
+        .map(s => s[0].toUpperCase())
+        .join('')
+
+    $(`[name="${to}"]`).val(val)
+}
