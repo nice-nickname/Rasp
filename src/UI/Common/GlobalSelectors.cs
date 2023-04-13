e20bbe9f92da6a2c82526fa536fab39c7c044c39
semen <bethesda.stillxxxx@gmail.com>
2023-04-13 09:53:16 +0300

    Добавил начальную страницу-плейсхолдер, добавил возможность выключить лоадер

--- src/UI/Common/GlobalSelectors.cs
+++ src/UI/Common/GlobalSelectors.cs
@@ -2,6 +2,8 @@
 
 public static class GlobalSelectors
 {
+    public static readonly string AjaxLoaderId = "76115CA4-C612-4AA6-8CFB-8D1AA896011F";
+
     public static readonly string FacultyId = "7BA4D038-A98B-4C5F-9D74-49113295939B";
 
     public static readonly string MainId = "D522480D-BBB0-4C1D-A020-4456F22598FD";
