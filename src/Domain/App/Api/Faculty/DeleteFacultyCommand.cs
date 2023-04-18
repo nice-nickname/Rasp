3fd8e9a4a535bc45c23c94220485258355bc3aa5
Sergey <kerss26rus@gmail.com>
2023-04-19 02:23:25 +0300

    Пофиксил невозможность редактирования существующих кафедр и инстиутов; Привёл страницу со списком институтов к общему виду

---
+++ src/Domain/App/Api/Faculty/DeleteFacultyCommand.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Persistence;
+using Incoding.Core.CQRS.Core;
+
+namespace Domain.Api;
+
+public class DeleteFacultyCommand : CommandBase
+{
+    public int Id { get; set; }
+
+    protected override void Execute()
+    {
+        Repository.Delete<Faculty>(Id);
+    }
+}
