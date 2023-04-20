3e71a0d70ad9066ae2732777f7aba21c0a2020af
semen <bethesda.stillxxxx@gmail.com>
2023-04-20 21:18:06 +0300

    Добавил настройки для института

--- src/UI/Controllers/MigrationsController.cs
+++ src/UI/Controllers/MigrationsController.cs
@@ -39,4 +39,33 @@ public class MigrationsController : Controller
     {
         return Ok($"migrated down to {number}");
     }
+
+    [Route("erase")]
+    public IActionResult Erase()
+    {
+        var sql = @"
+DECLARE @Sql NVARCHAR(500) DECLARE @Cursor CURSOR
+
+SET @Cursor = CURSOR FAST_FORWARD FOR
+SELECT DISTINCT sql = 'ALTER TABLE [' + tc2.TABLE_SCHEMA + '].[' +  tc2.TABLE_NAME + '] DROP [' + rc1.CONSTRAINT_NAME + '];'
+FROM INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS rc1
+LEFT JOIN INFORMATION_SCHEMA.TABLE_CONSTRAINTS tc2 ON tc2.CONSTRAINT_NAME =rc1.CONSTRAINT_NAME
+
+OPEN @Cursor FETCH NEXT FROM @Cursor INTO @Sql
+
+WHILE (@@FETCH_STATUS = 0)
+BEGIN
+Exec sp_executesql @Sql
+FETCH NEXT FROM @Cursor INTO @Sql
+END
+
+CLOSE @Cursor DEALLOCATE @Cursor
+GO
+
+EXEC sp_MSforeachtable 'DROP TABLE ?'
+GO";
+
+        this._migrator.Processor.Execute(sql);
+        return Ok("all data has been erased");
+    }
 }
