97b9f4b10de07add1d51997d32a126a601d27ce4
semen <bethesda.stillxxxx@gmail.com>
2023-04-30 10:55:51 +0300

    Создал таблицу для предпочтений преподавателей по парам

---
+++ src/Domain/Persistence/Migrations/Step12.cs
@@ -0,0 +1,35 @@
+﻿using System.Data;
+using Domain.Extensions;
+using FluentMigrator;
+
+namespace Domain.Persistence;
+
+[Migration(12, "Added teacher preferences")]
+public class Step12 : Migration
+{
+    public override void Up()
+    {
+        Create.Table(nameof(TeacherPreferences))
+              .WithColumn(nameof(TeacherPreferences.Id)).AsIntPK()
+              .WithColumn(nameof(TeacherPreferences.TeacherId)).AsInt32()
+              .WithColumn(nameof(TeacherPreferences.ScheduleFormatId)).AsInt32()
+              .WithColumn(nameof(TeacherPreferences.Type)).AsByte();
+
+        Create.ForeignKey("FK_TeacherPreferences_Teacher")
+              .FromTable(nameof(TeacherPreferences)).ForeignColumn(nameof(TeacherPreferences.TeacherId))
+              .ToTable(nameof(Teacher)).PrimaryColumn(nameof(Teacher.Id))
+              .OnDeleteOrUpdate(Rule.None);
+
+        Create.ForeignKey("FK_TeacherPreferences_ScheduleFormat")
+              .FromTable(nameof(TeacherPreferences)).ForeignColumn(nameof(TeacherPreferences.ScheduleFormatId))
+              .ToTable(nameof(ScheduleFormat)).PrimaryColumn(nameof(ScheduleFormat.Id))
+              .OnDeleteOrUpdate(Rule.None);
+    }
+
+    public override void Down()
+    {
+        Delete.ForeignKey("FK_TeacherPreferences_Teacher").OnTable(nameof(TeacherPreferences));
+        Delete.ForeignKey("FK_TeacherPreferences_ScheduleFormat").OnTable(nameof(TeacherPreferences));
+        Delete.Table(nameof(TeacherPreferences));
+    }
+}
---
+++ src/Domain/Persistence/Models/TeacherPreferences.cs
@@ -0,0 +1,45 @@
+﻿using FluentNHibernate.Mapping;
+using Incoding.Core.Data;
+
+namespace Domain.Persistence;
+
+public class TeacherPreferences : IncEntityBase
+{
+    public enum PreferenceType
+    {
+        Impossible,
+
+        Unwanted
+    }
+
+    public new virtual int Id { get; set; }
+
+    public virtual int TeacherId { get; set; }
+
+    public virtual int ScheduleFormatId { get; set; }
+
+    public virtual PreferenceType Type { get; set; }
+
+    public virtual Teacher Teacher { get; set; }
+
+    public virtual ScheduleFormat ScheduleFormat { get; set; }
+
+    public class Mapping : ClassMap<TeacherPreferences>
+    {
+        public Mapping()
+        {
+            Id(s => s.Id).GeneratedBy.Identity();
+            Map(s => s.TeacherId);
+            Map(s => s.ScheduleFormatId);
+            Map(s => s.Type).CustomType<PreferenceType>();
+
+            References(s => s.Teacher).Column(nameof(TeacherId))
+                                      .ReadOnly()
+                                      .LazyLoad();
+
+            References(s => s.ScheduleFormat).Column(nameof(ScheduleFormatId))
+                                             .ReadOnly()
+                                             .LazyLoad();
+        }
+    }
+}
