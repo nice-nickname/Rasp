f882fcac9e5a156994b75267600adb4217584758
semen <bethesda.stillxxxx@gmail.com>
2023-05-05 00:18:57 +0300

    SC-119: Добавил таблицу для праздничных дней

---
+++ src/Domain/Persistence/Models/Holidays.cs
@@ -0,0 +1,23 @@
+﻿using FluentNHibernate.Mapping;
+using Incoding.Core.Data;
+
+namespace Domain.Persistence;
+
+public class Holidays : IncEntityBase
+{
+    public new virtual int Id { get; set; }
+
+    public virtual DateOnly Date { get; set; }
+
+    public virtual string? Name { get; set; }
+
+    public class Mapping : ClassMap<Holidays>
+    {
+        public Mapping()
+        {
+            Id(s => s.Id).GeneratedBy.Identity();
+            Map(s => s.Name).Nullable();
+            Map(s => s.Date).CustomType<Mappers.NHibernateDateOnlyType>();
+        }
+    }
+}
