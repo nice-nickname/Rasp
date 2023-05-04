825882888d39e36bbed03226987949884de820e7
Sergey <kerss26rus@gmail.com>
2023-05-04 22:27:44 +0300

    SC-103: Обновил страницу редактирования данных, добавил поиск

--- src/UI/wwwroot/js/site.js
+++ src/UI/wwwroot/js/site.js
@@ -60,3 +60,16 @@ function suggestInputValueByNamingCase(value, to) {
         $(this).selectpicker('val', JSON.parse(selected))
     }
 }(jQuery));
+
+(function() {
+    $.fn.search = function(list) {
+        let search = $(this).val()
+
+        $(list).find('[filter]').each(function() {
+            if ($(this).attr('filter').toLowerCase().includes(search.toLowerCase()))
+                $(this).removeClass('hidden')
+            else
+                $(this).addClass('hidden')
+        })
+    }
+}(jQuery));
