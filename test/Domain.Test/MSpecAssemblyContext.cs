7a9aca5e88f6b1a74c0f97be89743dc7dd48aa3e
semen <bethesda.stillxxxx@gmail.com>
2023-05-05 01:01:36 +0300

    Добавил проект для модульного тестирования

---
+++ test/Domain.Test/MSpecAssemblyContext.cs
@@ -0,0 +1,23 @@
+﻿using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using Incoding.Data.NHibernate;
+using Incoding.UnitTests.MSpec;
+using Machine.Specifications;
+using NHibernate.Tool.hbm2ddl;
+
+namespace Domain.Test;
+
+public class MSpecAssemblyContext : IAssemblyContext
+{
+    public void OnAssemblyStart()
+    {
+        var nhConfig = Fluently.Configure()
+                               .Database(MsSqlConfiguration.MsSql2008.ConnectionString("").ShowSql())
+                               .ExposeConfiguration(cfg => new SchemaUpdate(cfg).Execute(false, true))
+                               .Mappings(configuration => configuration.FluentMappings.AddFromAssembly(typeof(Bootstrap).Assembly));
+
+        PleasureForData.Factory = () => new NhibernateUnitOfWorkFactory(new NhibernateSessionFactory(nhConfig));
+    }
+
+    public void OnAssemblyComplete() { }
+}
