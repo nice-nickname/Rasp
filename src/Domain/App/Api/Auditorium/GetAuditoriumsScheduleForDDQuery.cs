b85e9e3330e99287135c29578c9354e33b1b8326
Sergey <kerss26rus@gmail.com>
2023-05-14 23:05:22 +0300

    SC-127: Добавил отображение предпочитаемых пар

--- src/Domain/App/Api/Auditorium/GetAuditoriumsScheduleForDDQuery.cs
+++ src/Domain/App/Api/Auditorium/GetAuditoriumsScheduleForDDQuery.cs
@@ -12,12 +12,24 @@ public class GetAuditoriumsScheduleForDDQuery : QueryBase<List<KeyValueVm>>
 
     public int Order { get; set; }
 
+    public int SubDisciplineId { get; set; }
+
     public int? AuditoriumId { get; set; }
 
     public DayOfWeek Day { get; set; }
 
     protected override List<KeyValueVm> ExecuteResult()
     {
+        var preferredAuditoriumIds = Repository.Query<SubDisciplineAuditoriums>()
+                                               .Where(r => r.SubDisciplineId == SubDisciplineId)
+                                               .Select(r => r.AuditoriumId)
+                                               .ToList();
+
+        var preferredKindIds = Repository.Query<SubDisciplineAuditoriumKinds>()
+                                         .Where(r => r.SubDisciplineId == SubDisciplineId)
+                                         .Select(r => r.AuditoriumKindId)
+                                         .ToList();
+
         var scheduledAuditoriums = Repository.Query<Class>()
                                              .Where(r => r.Day == Day
                                                       && r.ScheduleFormat.Order == Order
@@ -27,7 +39,10 @@ public class GetAuditoriumsScheduleForDDQuery : QueryBase<List<KeyValueVm>>
                                              .ToList();
 
         return Repository.Query<Auditorium>()
-                         .Where(r => r.Capacity >= Capacity)
+                         .ToList()
+                         .Where(r => (r.Capacity >= Capacity
+                                   && r.Kinds.Select(q => q.Id).Intersect(preferredKindIds).Any())
+                                  || preferredAuditoriumIds.Contains(r.Id))
                          .Select(r => new KeyValueVm
                          {
                                  Text = $"{r.Building.Name}-{r.Code}",
