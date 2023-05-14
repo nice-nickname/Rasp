902a871688a759cb01f4357a81537a4f20880cfe
semen <bethesda.stillxxxx@gmail.com>
2023-05-14 14:29:06 +0300

    SC-33: Страница поиска

---
+++ src/Domain/App/Api/Export/GetExportSearchQuery.cs
@@ -0,0 +1,62 @@
+﻿using Domain.Persistence;
+using Incoding.Core.CQRS.Core;
+
+namespace Domain.Api;
+
+public class GetExportSearchQuery : QueryBase<List<GetExportSearchQuery.Response>>
+{
+    public string Search { get; set; }
+
+    protected override List<Response> ExecuteResult()
+    {
+        var teachers = Repository.Query<Teacher>()
+                                 .Where(s => s.Name.Contains(Search))
+                                 .Select(s => new Response
+                                 {
+                                         Id = s.Id,
+                                         Name = s.Name,
+                                         Type = OfType.TEACHER
+                                 })
+                                 .ToList();
+
+        var groups = Repository.Query<Group>()
+                               .Where(s => s.Code.Contains(Search))
+                               .Select(s => new Response
+                               {
+                                       Id = s.Id,
+                                       Name = s.Code,
+                                       Type = OfType.GROUP
+                               })
+                               .ToList();
+
+        var auditoriums = Repository.Query<Auditorium>()
+                                    .Where(s => s.Code.Contains(Search))
+                                    .Select(s => new Response
+                                    {
+                                            Id = s.Id,
+                                            Name = $"{s.Building.Name}-{s.Code}",
+                                            Type = OfType.AUDITORIUM
+                                    })
+                                    .ToList();
+
+        teachers.AddRange(groups);
+        teachers.AddRange(auditoriums);
+        return teachers;
+    }
+
+    public record Response
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public OfType Type { get; set; }
+    }
+
+    public enum OfType
+    {
+        TEACHER,
+        GROUP,
+        AUDITORIUM
+    }
+}
---
+++ src/UI/Controllers/ExportController.cs
@@ -0,0 +1,37 @@
+﻿using Domain.Api;
+using Incoding.Core.CQRS.Core;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UI.Controllers;
+
+[AllowAnonymous]
+[Route("export")]
+public class ExportController : Controller
+{
+    private readonly IDispatcher _dispatcher;
+
+    public ExportController(IDispatcher dispatcher)
+    {
+        this._dispatcher = dispatcher;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [Route("/schedule/{type}/{id}")]
+    public IActionResult Schedule([FromRoute] GetExportSearchQuery.OfType type, [FromRoute] int id, [FromQuery] int? week)
+    {
+        var result = this._dispatcher.Query(new GetScheduleByWeekQuery
+        {
+                Week = week.GetValueOrDefault(1),
+                FacultyId = Convert.ToInt32(HttpContext.Request.Cookies[GlobalSelectors.FacultyId]),
+                SelectedAuditoriumId = type == GetExportSearchQuery.OfType.AUDITORIUM ? id : null,
+                SelectedTeacherId = type == GetExportSearchQuery.OfType.TEACHER ? id : null,
+                SelectedGroupId = type == GetExportSearchQuery.OfType.GROUP ? id : null,
+        });
+        return View(result);
+    }
+}
