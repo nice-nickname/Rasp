adc9bbb5b77bb443f64d96023334a32e8f6eb76a
Sergey <kerss26rus@gmail.com>
2023-05-18 18:16:47 +0300

    Пофиксил очистку расписания

--- src/Domain/App/Api/Class/DeleteClassesInWeekCommand.cs
+++ src/Domain/App/Api/Class/DeleteClassesInWeekCommand.cs
@@ -7,33 +7,49 @@ public class DeleteClassesInWeekCommand : CommandBase
 {
     public int Week { get; set; }
 
-    public int FacultyId { get; set; }
+    public int?[] SelectedGroupIds { get; set; }
 
-    public int? SelectedGroupId { get; set; }
+    public int?[] SelectedAuditoriumIds { get; set; }
 
-    public int? SelectedAuditoriumId { get; set; }
-
-    public int? SelectedTeacherId { get; set; }
+    public int?[] SelectedTeacherIds { get; set; }
 
     protected override void Execute()
     {
-        if (!SelectedAuditoriumId.HasValue
-            && !SelectedGroupId.HasValue
-            && !SelectedTeacherId.HasValue)
+        if (SelectedGroupIds.FirstOrDefault() == null
+         && SelectedAuditoriumIds.FirstOrDefault() == null
+         && SelectedTeacherIds.FirstOrDefault() == null)
             return;
+
+        var isSingle = SelectedGroupIds.Length == 1
+                    && SelectedAuditoriumIds.Length == 1
+                    && SelectedTeacherIds.Length == 1;
 
         var scheduledList = Repository.Query<Class>()
                                       .Where(r => r.Week == Week)
                                       .ToList();
 
-        if (SelectedGroupId.HasValue)
-            scheduledList = scheduledList.Where(r => r.Plan.GroupId == SelectedGroupId).ToList();
+        if (isSingle)
+        {
+            if (SelectedGroupIds.FirstOrDefault() != null)
+                scheduledList = scheduledList.Where(r => r.Plan.GroupId == SelectedGroupIds.First()).ToList();
 
-        if (SelectedAuditoriumId.HasValue)
-            scheduledList = scheduledList.Where(r => r.AuditoriumId == SelectedAuditoriumId).ToList();
+            if (SelectedAuditoriumIds.FirstOrDefault() != null)
+                scheduledList = scheduledList.Where(r => r.AuditoriumId == SelectedAuditoriumIds.First()).ToList();
 
-        if (SelectedTeacherId.HasValue)
-            scheduledList = scheduledList.Where(r => r.Plan.TeacherId == SelectedTeacherId).ToList();
+            if (SelectedTeacherIds.FirstOrDefault() != null)
+                scheduledList = scheduledList.Where(r => r.Plan.TeacherId == SelectedTeacherIds.First()).ToList();
+        }
+        else
+        {
+            if (SelectedGroupIds.Length > 1)
+                scheduledList = scheduledList.Where(r => SelectedGroupIds.Contains(r.Plan.GroupId)).ToList();
+
+            if (SelectedAuditoriumIds.Length > 1)
+                scheduledList = scheduledList.Where(r => SelectedAuditoriumIds.Contains(r.AuditoriumId)).ToList();
+
+            if (SelectedTeacherIds.Length > 1)
+                scheduledList = scheduledList.Where(r => SelectedTeacherIds.Contains(r.Plan.TeacherId)).ToList();
+        }
 
         foreach (var scheduled in scheduledList)
             Dispatcher.Push(new DeleteClassCommand { Id = scheduled.Id });
