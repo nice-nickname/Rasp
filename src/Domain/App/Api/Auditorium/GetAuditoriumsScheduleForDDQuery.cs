673713ecf58e7f7965125d1d0771f73ec7cf3531
semen <bethesda.stillxxxx@gmail.com>
2023-05-27 14:45:21 +0300

    SC-167: Оптимизация запроса на выбор подходящих аудиторий

--- src/Domain/App/Api/Auditorium/GetAuditoriumsScheduleForDDQuery.cs
+++ src/Domain/App/Api/Auditorium/GetAuditoriumsScheduleForDDQuery.cs
@@ -8,49 +8,61 @@ public class GetAuditoriumsScheduleForDDQuery : QueryBase<List<KeyValueVm>>
 {
     public int Week { get; set; }
 
-    public int Order { get; set; }
+    public int ScheduleFormatId { get; set; }
 
     public int SubDisciplineId { get; set; }
 
     public int? AuditoriumId { get; set; }
 
+    public int? GroupId { get; set; }
+
     public DayOfWeek Day { get; set; }
 
     protected override List<KeyValueVm> ExecuteResult()
     {
-        var preferredAuditoriumIds = Repository.Query<SubDisciplineAuditoriums>()
-                                               .Where(r => r.SubDisciplineId == SubDisciplineId)
-                                               .Select(r => r.AuditoriumId)
-                                               .ToList();
+        var subDiscipline = Repository.GetById<SubDiscipline>(SubDisciplineId);
 
-        var preferredKindIds = Repository.Query<SubDisciplineAuditoriumKinds>()
-                                         .Where(r => r.SubDisciplineId == SubDisciplineId)
-                                         .Select(r => r.AuditoriumKindId)
-                                         .ToList();
+        var preferredKindIds = subDiscipline.AuditoriumKinds.Select(r => r.Id).ToList();
+        var preferredBySelf = subDiscipline.Auditoriums.Select(r => r.Id).ToList();
+
+        var preferredByKind = Repository.Query<AuditoriumToKinds>()
+                                    .Where(s => preferredKindIds.Contains(s.AuditoriumKindId))
+                                    .Select(s => s.AuditoriumId);
 
         var scheduledAuditoriums = Repository.Query<Class>()
                                              .Where(r => r.Day == Day
-                                                      && r.ScheduleFormat.Order == Order
+                                                      && r.ScheduleFormatId == ScheduleFormatId
                                                       && r.Week == Week
                                                       && r.AuditoriumId != AuditoriumId)
                                              .Select(r => r.AuditoriumId)
                                              .ToList();
 
-        var capacity = Repository.Query<Class>()
-                                 .Where(r => r.Plan.SubDisciplineId == SubDisciplineId)
-                                 .Sum(r => r.Plan.Group.StudentCount);
+        var capacity = subDiscipline.IsParallelHours 
+                            ? subDiscipline.Discipline.Groups.Sum(s => s.StudentCount) 
+                            : Repository.GetById<Group>(GroupId).StudentCount;
 
-        return Repository.Query<Auditorium>()
-                         .ToList()
-                         .Where(r => (r.Capacity >= capacity
-                                   && (preferredKindIds.Count <= 0 || r.Kinds.Select(q => q.Id).Intersect(preferredKindIds).Any()))
-                                  || preferredAuditoriumIds.Contains(r.Id))
-                         .Select(r => new KeyValueVm
-                         {
-                                 Text = $"{r.Building.Name}-{r.Code}",
-                                 Value = r.Id.ToString(),
-                                 CssClass = scheduledAuditoriums.Contains(r.Id) ? "disabled" : string.Empty
-                         })
-                         .ToList();
+        var result = Repository.Query<Auditorium>();
+
+        if (preferredBySelf.Any())
+        {
+            result = result.Where(s => preferredBySelf.Contains(s.Id));
+        }
+
+        if (preferredByKind.Any())
+        {
+            result = result.Where(s => s.Capacity >= capacity && preferredByKind.Contains(s.Id));
+        }
+        else
+        {
+            result = result.Where(s => s.Capacity >= capacity);
+        }
+
+        return result.Select(r => new KeyValueVm
+                     {
+                            Text = $"{r.Building.Name}-{r.Code}",
+                            Value = r.Id.ToString(),
+                            CssClass = scheduledAuditoriums.Contains(r.Id) ? "disabled" : string.Empty
+                     })
+                     .ToList();
     }
 }
