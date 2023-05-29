267c148f0cb3ea3d63173b551832b21586b96ff4
Sergey <kerss26rus@gmail.com>
2023-05-29 22:15:35 +0300

    SC-152: Добавил кнопку для страниц составления расписания, редактирования данных и страницы параметров

--- src/UI/wwwroot/js/drag-and-drop.js
+++ src/UI/wwwroot/js/drag-and-drop.js
@@ -82,10 +82,13 @@
             function onMouseDown(ev) {
                 ev.preventDefault()
 
+                console.log(ev.target)
+
                 if ($(ev.target).is('select')
                     || $(ev.target).is('input')
                     || $(ev.target).is('button')
                     || $(ev.target).is('a.dropdown-item')
+                    
                     || $(ev.target).closest('button').length != 0) {
                     return
                 }
